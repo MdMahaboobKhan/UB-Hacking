c7ca73f069c832b8112aa352abca5a3a7fe73dc3
MdMahaboobkhan <khan.m.mahaboob@gmail.com>
2020-10-24 20:33:43 -0400

    home screen - react

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -2,7 +2,44 @@ import React from 'react'
 
 const Home = () =>{
     return(
-        <h1>home</h1>
+        <div className="home">
+            <div className="card home-card">
+                <h5>Sufiyan Khan</h5>
+                <div className="card-image">
+                    <img src="https://images.unsplash.com/photo-1557238687-09eed2ad0522?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"/>
+                </div>
+                <div className="card-content">
+                    <i class="material-icons" style={{color:"red"}}>favorite</i>
+                    <h6>title</h6>
+                    <p>this is an amazing post</p>
+                    <input type="text" placeholder="add a comment"/>
+                </div>
+            </div>
+            <div className="card home-card">
+                <h5>Sufiyan Khan</h5>
+                <div className="card-image">
+                    <img src="https://images.unsplash.com/photo-1557238687-09eed2ad0522?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"/>
+                </div>
+                <div className="card-content">
+                    <i class="material-icons" style={{color:"red"}}>favorite</i>
+                    <h6>title</h6>
+                    <p>this is an amazing post</p>
+                    <input type="text" placeholder="add a comment"/>
+                </div>
+            </div>
+            <div className="card home-card">
+                <h5>Sufiyan Khan</h5>
+                <div className="card-image">
+                    <img src="https://images.unsplash.com/photo-1557238687-09eed2ad0522?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"/>
+                </div>
+                <div className="card-content">
+                    <i class="material-icons" style={{color:"red"}}>favorite</i>
+                    <h6>title</h6>
+                    <p>this is an amazing post</p>
+                    <input type="text" placeholder="add a comment"/>
+                </div>
+            </div>
+        </div>
     )
 }
 
